6354148c63ba14c08a4764a59a2cf483d57941b4
xxtars <49916044+xxtars@users.noreply.github.com>
2026-02-14 21:53:33 +0000

    Deploying to gh-pages from @ xxtars/xxtars.github.io@c75ee0f7a85c7f11e5bb60ebe91d3a36afd6be07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-joined-lut-university-as-a-doctoral-researcher-in-the-computer-vision-and-pattern-recognition-laboratory-cvprl",
+          title: 'I joined LUT University as a Doctoral Researcher in the Computer Vision and...',
+          description: "",
+          section: "News",},{id: "news-one-paper-on-mllm-for-identity-free-emotion-understanding-was-accepted-to-acm-multimedia-2025",
+          title: 'One paper on MLLM for identity-free emotion understanding was accepted to ACM Multimedia...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
