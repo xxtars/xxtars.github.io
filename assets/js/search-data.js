1f6989151afb29bfc2a179272a2f610ce1519127
xxtars <49916044+xxtars@users.noreply.github.com>
2026-02-14 21:59:28 +0000

    Deploying to gh-pages from @ xxtars/xxtars.github.io@1db078cca4d3d19d834cf87a806e0bf737cc35a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,14 +398,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-joined-lut-university-as-a-doctoral-researcher-in-the-computer-vision-and-pattern-recognition-laboratory-cvprl",
+            },},{id: "news-i-joined-lut-university-as-a-doctoral-researcher-in-the-computer-vision-and-pattern-recognition-laboratory-cvprl",
           title: 'I joined LUT University as a Doctoral Researcher in the Computer Vision and...',
           description: "",
           section: "News",},{id: "news-one-paper-on-mllm-for-identity-free-emotion-understanding-was-accepted-to-acm-multimedia-2025",
           title: 'One paper on MLLM for identity-free emotion understanding was accepted to ACM Multimedia...',
+          description: "",
+          section: "News",},{id: "news-one-paper-on-mamba-for-micro-gesture-recognition-is-accepted-by-ieee-t-mm",
+          title: 'One paper on Mamba for Micro-Gesture Recognition is accepted by IEEE T-MM.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
