cc2ca13b369706c2c8ad28d387594c28ac76dfe2
xxtars <49916044+xxtars@users.noreply.github.com>
2026-02-14 20:53:26 +0000

    Deploying to gh-pages from @ xxtars/xxtars.github.io@a2b2f46da969110469eab0eb5a39f910a030bcac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "",
+          description: "publications by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
