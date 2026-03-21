af16cce406de512cf1fc186a7f873f6833c35544
xxtars <49916044+xxtars@users.noreply.github.com>
2026-03-21 19:28:07 +0000

    Deploying to gh-pages from @ xxtars/xxtars.github.io@56823cac5e3e0b4049ea37740b0d77e9776dc37f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -486,6 +486,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=CzmYh2EAAAAJ", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/bohao-xing-2780273b9", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
